81c05bdf995242e7cc4e8e1dc7e693dbad9e43bf
Corey Bradshaw <cjabradshaw@gmail.com>
2023-08-18 21:47:53 +0930

    Update Invasion_terms.R

--- scripts/Invasion_terms.R
+++ scripts/Invasion_terms.R
@@ -1,173 +1,184 @@
-
 # Invasion terms 
 # Ismael Soto & Corey Bradshaw
+# August 2023
 
-#####################################   Formatting   ################################################
-library(readxl)
-df = read_xlsx('WoS_Search_Terms_31July2023.xlsx') #From Fran 
+##################################
+## without 'species' tag included 
+##################################
+# import data
+dat1 <- read.csv("termsWOspp.csv", header=T)
+head(dat1)
+tail(dat1)
 
-names(df)
+# fix term names
+year.seq <- seq(1,117,2)
+count.seq <- seq(2,118,2)
 
-df1 = df[,c(3,17,26,48, 51, 66)]
-head(df1)
-unique(df1$`WoS Categories`) #
+terms.vec <- colnames(dat1)[count.seq]
+  terms.vec[1] <- "acclimatized"
+  terms.vec[10] <- "colonizer"
+  terms.vec[12] <- "domestic (iea)"
+  terms.vec[22] <- "intra-country established alien"
+  terms.vec[26] <- "invasive alien"
+  terms.vec[27] <- "invasive non-native"
+  terms.vec[28] <- "invasive super-dominant"
+  terms.vec[30] <- "naturalized"
+  terms.vec[33] <- "new non-native"
+  terms.vec[36] <- "non-indigenous"
+  terms.vec[37] <- "non-native"
+  terms.vec[38] <- "non-resident"
+  terms.vec[44] <- "range-expanding"
+colnames(dat1)[count.seq] <- terms.vec
+head(dat1)
+colnames(dat1)[year.seq] <- "year"
+head(dat1)
 
-unique(df1$Term) # 55 Terms + invasive super dominant?,  native-alien 
+# separate terms into unique data.frames
+for (i in 1:length(terms.vec)) {
+  assign(paste0(terms.vec[i],".dat"), dat1[,year.seq[i]:count.seq[i]])
+}
 
-sp_to_remove = c('ruderal plant','native-alien species')
+# merge data frames by year
+min.yr <- range(dat1[,year.seq], na.rm=T)[1]
+max.yr <- range(dat1[,year.seq], na.rm=T)[2]
+year.vec <- seq(min.yr, max.yr, 1)
+year.dat <- data.frame(year.vec, rep(NA, length(year.vec)))
+colnames(year.dat) <- c("year", "dummy")
 
-df1= df1 %>% filter(!Term %in% sp_to_remove)
+terms.dat.vec <- paste(terms.vec, ".dat", sep="")
+for (s in 1:length(terms.dat.vec)) {
+  dat1.it <- get0(terms.dat.vec[s])
+  year.dat <- merge(year.dat, dat1.it, by="year", all.x=T, no.dups=T)
+}
+head(year.dat)
+tail(year.dat)
 
-keywords <- c("Ecology", "Environmental", "Biodiversity", 'Plant Sciences','Fisheries',
-              "Marine & Freshwater Biology", "Zoology", "Botany", "Agricultur")
+# average & interquartile range of # papers by year
+dat.seq <- year.dat[,-2]
+mean.count <- apply(dat.seq[2:dim(dat.seq)[2]], MARGIN=1, mean, na.rm=T)
+up.count <- apply(dat.seq[2:dim(dat.seq)[2]], MARGIN=1, quantile, probs=0.75, na.rm=T)
+lo.count <- apply(dat.seq[2:dim(dat.seq)[2]], MARGIN=1, quantile, probs=0.25, na.rm=T)
+sd.count <- apply(dat.seq[2:dim(dat.seq)[2]], MARGIN=1, sd, na.rm=T)
+mean.dat <- data.frame(year.vec, mean.count, up.count, lo.count, sd.count)
 
-df_filtered <- df1 %>%
-  filter(str_detect(`WoS Categories`, str_c(keywords, collapse = "|")))
+write.table(mean.dat, "meanOut.csv", sep=",", col.names = T, row.names = F)
 
+# term diversity through time
+term.bin <- ifelse(dat.seq >= 1, 1, NA)
+term.div <- apply(term.bin, MARGIN=1, sum, na.rm=T)
+term.div.r <- c(NA, log(term.div[2:length(term.div)] / term.div[1:(length(term.div)-1)]))
+termDiv.out <- data.frame(year.vec, term.div, term.div.r)
 
-#Extract the ecosystem
-df_filtered <- df_filtered %>% 
-  mutate(Ecosystem = case_when(
-    str_detect(Abstract, "marine") ~ "marine",
-    str_detect(Abstract, "freshwater") ~ "freshwater",
-    str_detect(Abstract, "terrestrial") ~ "terrestrial",
-    TRUE ~ "NA"
-  ))
+write.table(termDiv.out, "termDiv.csv", sep=",", col.names = T, row.names = F)
 
-table(df_filtered$Ecosystem) # many NAs...
-table(df_filtered$Language)
+# cumulative term diversity
+term.cum <- term.bin
+for (j in 1:length(terms.vec)) {
+  first.appear <- which(is.na(term.bin[,j]) == F)[1]
+  if (is.na(first.appear) == F) {
+    term.cum[first.appear:dim(term.bin)[1], j] <- 1
+  }
+}
+term.cumsum <- apply(term.cum, MARGIN=1, sum, na.rm=T)
+termDivCS.out <- data.frame(year.vec, term.cumsum)
 
-names(df_filtered)
+write.table(termDivCS.out, "termDivCS.csv", sep=",", col.names = T, row.names = F)
 
-df2 <- df_filtered %>%
-  group_by(Term, `Publication Year`) %>%
-  summarise(N_papers = n(), .groups = 'drop')
-
-#Total number of papers
-Total= df2 %>% group_by(Term)  %>% summarise(sum = sum(N_papers))
-
-#Relative n papers
-df3 <- df2 %>%
-  group_by(`Publication Year`) %>%
-  mutate(Relative_N_papers = N_papers / sum(N_papers))
-
-terms= c('alien species',"exotic species","non-native species","invasive/invader species","non-indigenous species")
-df4= df3 %>% filter(Term %in% terms)
-
-
-df4 %>%
-  ggplot(aes(x = `Publication Year`, y = Relative_N_papers, color = Term)) +
-  geom_line(size= 1.5) +
-  labs(title = "Temporal Trends in the Relative Importance of Terms",
-       x = "Publication Year",
-       y = "Relative Number of Papers",
-       color = "Term") +
-  theme_minimal() + xlim(1990,2020)+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-################################################################################################################
-
-## call libraries
+# word cloud
 library(wordcloud)
 library(RColorBrewer)
 library(wordcloud2)
 library(tm)
+library(pals)
 
-## import data
-setwd("~/Documents/Papers/Invasive species/Terminology/data")
+dat2 <- read.csv("totalTermCount.csv", header=T)
 
-
-head(df2)
-unique(df2$Term) #Note that vermin and xenobiota species has only one paper (no detected by my previous filters in WOS, 
-# because their category is History and Microbiology, respectively). However as they have only paper makes no sense to plot it 
-
-# I need to make these change to the wordcloud
-df2$Term[df2$Term=='invasive/invader species' ] = 'invasive species'
-df2$Term[df2$Term=='colonizer/colonist' ] = 'colonizer'
-
-colnames(df2) <- c('term', 'year', 'count')
-unique(df2$term)
-
-df2$term <- gsub(" species|$", "", df2$term)
-df2$term <- trimws(df2$term)
-
-
-#dat <- readxl::read_excel("terms.xlsx") From Corey 
-dat <- df2
-
-
-## term diversity with time
-termfreq <- table(dat$term, dat$year)
-termfreqTS <- as.data.frame(apply(termfreq, MARGIN=2, sum, na.rm=T))
-colnames(termfreqTS) <- c("nterms")
-termfreqTS$year <- as.numeric(rownames(termfreqTS))
-head(termfreqTS)
-plot(termfreqTS$year, termfreqTS$nterms, type="l", xlab="year", ylab="term diversity")
-termfreqTS$ntermR <- c(NA,log(termfreqTS$nterms[2:dim(termfreqTS)[1]] / termfreqTS$nterms[1:(dim(termfreqTS)[1] - 1)]))
-head(termfreqTS)
-plot(termfreqTS$year, termfreqTS$ntermR, type="l", xlab="year", ylab="term diversity r")
-write.table(termfreqTS,"termfreqTS.csv", row.names = F, sep=",")
-
-## r vs. n
-plot(termfreqTS$nterms[1:(dim(termfreqTS)[1]-1)], termfreqTS$ntermR[2:dim(termfreqTS)[1]], pch=19, xlab="term diversity", ylab="terms rate of change")
-
-## single terms
-term.vec <- attr(table(dat$term), "names")
-for (i in 1:length(term.vec)) {
-  assign(paste0(term.vec[i],".dat"), subset(dat, term == term.vec[i]))
-}
-
-termDat.vec <- paste0(term.vec,".dat")
-
-## plots
-attach(get(termDat.vec[1]))
-dat.use <- data.frame(year,count)
-datuse <- dat.use[order(dat.use[,1], decreasing=F),]
-plot(datuse$year, log10(datuse$count), type="l", xlab="year", ylab="log10 count", 
-     ylim=c(0,log10(max(dat$count,na.rm=T))), col="grey")
-detach(get(termDat.vec[1]))
-
-for (j in 2:length(termDat.vec)) {
-  attach(get(termDat.vec[j]))
-  dat.use <- data.frame(year,count)
-  datuse <- dat.use[order(dat.use[,1], decreasing=F),]
-  lines(datuse$year, log10(datuse$count), col="grey")
-  detach(get(termDat.vec[j]))
-}
-
-sumcounts <- as.data.frame(xtabs(dat$count ~ dat$year))
-dim(sumcounts)
-dim(termfreqTS)
-termfreqTS$countSum <- sumcounts$Freq
-termfreqTS$countMn <- termfreqTS$countSum/termfreqTS$nterms
-head(termfreqTS)
-lines(termfreqTS$year, log10(termfreqTS$countMn), lwd=3, lty=1, col="black")
-
-## create list with single-term data
-N <- length(termDat.vec)
-term.list <- vector("list", N)
-for (s in 1:N) {
-  Ps <- get(termDat.vec[s])
-  term.list[[s]] <- Ps
-}
-setNames(term.list, term.vec)
-
-sink("termList.txt")
-print(term.list)
-sink()
-
-
-## word cloud
-term.mat <- as.matrix(table(dat$count, dat$term))
-dim(term.mat)
-termWt.mat <- sort(unique(dat$count)) * term.mat
-head(termWt.mat)
-
-termcounts.mat <- as.matrix(apply(termWt.mat, MARGIN=2, sum))
-ISterms <- sort(rowSums(termcounts.mat), decreasing=T)
-IStermsDF <- data.frame(term = names(ISterms), freq=ISterms)
 set.seed(1234) # for reproducibility 
-wordcloud(words = IStermsDF$term, freq = IStermsDF$freq, min.freq = 1, max.words=35,
-          random.order=FALSE, rot.per=0.35, colors=brewer.pal(8, "Dark2"))
+wordcloud(words = dat2$term, freq = dat2$count, min.freq = 5, max.words=40,
+          random.order=FALSE, random.color=T, rot.per=0.20, colors=alphabet2(n = 26))
 
+
+################################
+## with 'species' tag included 
+################################
+dat3 <- read.csv("termsWspp.csv", header=T)
+head(dat3)
+tail(dat3)
+str(dat3)
+
+# fix term names
+dim(dat3)
+year.seq <- seq(1,97,2)
+count.seq <- seq(2,98,2)
+
+terms.vec <- colnames(dat3)[count.seq]
+terms.vec[22] <- "invasive alien"
+terms.vec[23] <- "invasive non-native"
+terms.vec[27] <- "new non-native"
+terms.vec[30] <- "non-indigenous"
+terms.vec[31] <- "non-native"
+terms.vec[32] <- "non-resident"
+terms.vec[38] <- "range-expanding"
+colnames(dat1)[count.seq] <- terms.vec
+head(dat3)
+colnames(dat3)[year.seq] <- "year"
+head(dat3)
+
+# separate terms into unique data.frames
+for (i in 1:length(terms.vec)) {
+  assign(paste0(terms.vec[i],".dat"), dat3[,year.seq[i]:count.seq[i]])
+}
+
+# merge data frames by year
+min.yr <- range(dat3[,year.seq], na.rm=T)[1]
+max.yr <- range(dat3[,year.seq], na.rm=T)[2]
+year.vec <- seq(min.yr, max.yr, 1)
+year.dat <- data.frame(year.vec, rep(NA, length(year.vec)))
+colnames(year.dat) <- c("year", "dummy")
+
+terms.dat.vec <- paste(terms.vec, ".dat", sep="")
+for (s in 1:length(terms.dat.vec)) {
+  dat3.it <- get0(terms.dat.vec[s])
+  year.dat <- merge(year.dat, dat3.it, by="year", all.x=T, no.dups=T)
+}
+head(year.dat)
+tail(year.dat)
+
+# average & interquartile range of # papers by year
+dat.seq <- year.dat[,-2]
+mean.count <- apply(dat.seq[2:dim(dat.seq)[2]], MARGIN=1, mean, na.rm=T)
+up.count <- apply(dat.seq[2:dim(dat.seq)[2]], MARGIN=1, quantile, probs=0.75, na.rm=T)
+lo.count <- apply(dat.seq[2:dim(dat.seq)[2]], MARGIN=1, quantile, probs=0.25, na.rm=T)
+sd.count <- apply(dat.seq[2:dim(dat.seq)[2]], MARGIN=1, sd, na.rm=T)
+mean.dat <- data.frame(year.vec, mean.count, up.count, lo.count, sd.count)
+
+write.table(mean.dat, "meanOutWspp.csv", sep=",", col.names = T, row.names = F)
+
+# term diversity through time
+term.bin <- ifelse(dat.seq >= 1, 1, NA)
+term.div <- apply(term.bin, MARGIN=1, sum, na.rm=T)
+term.div.r <- c(NA, log(term.div[2:length(term.div)] / term.div[1:(length(term.div)-1)]))
+termDiv.out <- data.frame(year.vec, term.div, term.div.r)
+
+write.table(termDiv.out, "termDivWspp.csv", sep=",", col.names = T, row.names = F)
+
+# cumulative term diversity
+term.cum <- term.bin
+for (j in 1:length(terms.vec)) {
+  first.appear <- which(is.na(term.bin[,j]) == F)[1]
+  if (is.na(first.appear) == F) {
+    term.cum[first.appear:dim(term.bin)[1], j] <- 1
+  }
+}
+term.cumsum <- apply(term.cum, MARGIN=1, sum, na.rm=T)
+termDivCS.out <- data.frame(year.vec, term.cumsum)
+
+write.table(termDivCS.out, "termDivCSwSPP.csv", sep=",", col.names = T, row.names = F)
+
+# word cloud
+dat4 <- read.csv("totalTermCountWspp.csv", header=T)
+
+set.seed(1234) # for reproducibility 
+wordcloud(words = dat4$term, freq = dat4$count, min.freq = 5, max.words=40,
+          random.order=FALSE, random.color=T, rot.per=0.20, colors=alphabet2(n = 26))
 
